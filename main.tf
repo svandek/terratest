9347f2cc69c6a7d2955bec71b477a6111e2ba847
Sander Klop <sander.klop@avanade.com>
2021-03-08 17:12:06 +0100

    edit storage name

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "rg-hello-azure-tf"
-    storage_account_name = "sahelloazuretf"
+    storage_account_name = "sahelloazuretf8765"
     container_name       = "terraform-state"
     key                  = "terraform.tfstate"
   }
